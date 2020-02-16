1c2ed913f66d9501a583cf498786982d29bf5399
Katy Solovewicz <katy@solovewicz.com>
2020-02-16 11:22:38 -0800

    add interactive search page

--- app.py
+++ app.py
@@ -16,63 +16,50 @@ Session(app)
 conn = sqlite3.connect('dmv2u.sqlite')
 cursor = conn.cursor()
 
-
 # Route to landing page
 @app.route("/", methods=['GET', 'POST'])
 def home():
-
 	return render_template("index.html")
 
-
 # Searches for plate status
-@app.route("/search/", methods=['POST'])
+@app.route("/quick-search/", methods=['POST'])
 def checkPlate():
-
-	search = request.form.get("search")
+	search = request.form.get("quick-search")
 	search = search.upper()
 	print("search: " + search)
-
 	record = helpers.select(search)
-
 	print("app record: " + str(record))
-
 	if record == "License Plate doesn\'t exist in the database":
 		return render_template("index.html", record=record)
-
 	else:
 		status = record[0].lower()
 		last = record[1]
-
 		print("status and last: " + status + " + " + last)
-
 		return render_template("index.html", search=search, status=status, last=last)
-
 		clear(search, status, last)
 		print("search, status, last: " + search + satus + last)
+
+# Returns table of available plates
+@app.route("/search/", methods=['GET', 'POST'])
+def searchPlates():
+	print("SEARCH PLATES")
+	return render_template("search.html")
 
 
 # Returns table of available plates
 @app.route("/available/", methods=['POST'])
 def availablePlates():
-
 	print("AVAILABLE PLATES!")
-
 	return render_template("index.html")
-
 
 # Returns table of restricted plates
 @app.route("/restricted/", methods=['POST'])
 def restrictedPlates():
-
 	print("RESTRICTED PLATES!")
-
 	return render_template("index.html")
-
 
 # Returns table of assigned plates
 @app.route("/assigned/", methods=['POST'])
 def assignedPlates():
-
 	print("ASSIGNED PLATES!")
-
 	return render_template("index.html")
